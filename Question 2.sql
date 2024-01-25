9a6470a2c63bb9c1ce87e9cd28a78afb5b9e8380
LenSedli <124786980+LenSedli@users.noreply.github.com>
2024-01-25 16:42:18 +0100

    Update Question 2.sql
    Signed-off-by: LenSedli <124786980+LenSedli@users.noreply.github.com>

--- Question 2.sql
+++ Question 2.sql
@@ -2,13 +2,14 @@ WITH price_value AS (
 SELECT
 	price_name,
 	price_year,
-	ROUND (payroll_value / price_value) AS value
+	ROUND (payroll_value / price_value) AS value,
+	price_unit
 FROM
 	t_lenka_sedlackova_project_sql_primary_final tlspspf
 WHERE
 	price_name IN ('Mléko polotučné pasterované', 'Chléb konzumní kmínový')
 	AND price_year IN (2007, 2018))
-SELECT price_name, price_year, sum (value)
+SELECT price_name, price_year, sum (value), price_unit
 FROM price_value
-GROUP BY price_name, price_year
+GROUP BY price_name, price_year, price_unit
 ;
